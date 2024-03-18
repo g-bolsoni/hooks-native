72ce523d1cac8b95bd4c9ed76a0bc68be9a8541b
Giovane Souza <giovane.souza@irroba.com.br>
2024-03-18 17:01:50 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useReducer, useRef } from 'react'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+const ActionsTypes = {
+  RESET: "RESET",
+  WRITE: "WRITE",
+
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ActionsTypes.RESET:
+      if (state.current) {
+        state.textInputRef.current.setNativeProps({ text: '' });
+      }
+      return state;
+
+    case ActionsTypes.WRITE:
+      state.textInputRef.current.setNativeProps({ text: 'Felipe' });
+      return state;
+
+    default:
+      break;
+  }
+}
 
 export default function App() {
+  const initialState = {
+    textInputRef: useRef<TextInput>(null),
+  }
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput
+        ref={textInputRef}
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+          marginBottom: 10,
+          marginTop: 20,
+          padding: 10
+        }}
+      >
+
+      </TextInput>
+
+      <Button
+        title='Reset'
+        onPress={resetButton}
+      />
       <StatusBar style="auto" />
     </View>
   );
